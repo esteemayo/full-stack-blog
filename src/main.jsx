fe9206d19d8f64e32db6895fbdb5eb9f2e03928d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-24 16:56:07 +0100

    feat: hande page routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import PostList from './routes/PostList';
+import Login from './routes/Login';
+import Register from './routes/Register';
+import Write from './routes/Write';
+import SinglePost from './routes/SinglePost';
+import Home from './routes/Home.jsx';
+
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: 'posts',
+    element: <PostList />,
+  },
+  {
+    path: ':slug',
+    element: <SinglePost />,
+  },
+  {
+    path: 'write',
+    element: <Write />,
+  },
+  {
+    path: 'login',
+    element: <Login />,
+  },
+  {
+    path: 'register',
+    element: <Register />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
