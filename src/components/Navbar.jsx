4eaabc0a39277fc3a68d43ae1d75cf9dae237a00
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-24 17:52:47 +0100

    fix: modify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
 
 import Image from './Image';
 
@@ -37,7 +38,7 @@ const Navbar = () => {
           <Link to='/'>Most popular</Link>
           <Link to='/'>About</Link>
           <Link to='/'>
-            <button type='button' className='py-2 px-4 rounded-3xl bg-blue-800'>
+            <button type='button' className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>
               Login 👋
             </button>
           </Link>
@@ -48,11 +49,16 @@ const Navbar = () => {
         <Link to='/'>Trending</Link>
         <Link to='/'>Most popular</Link>
         <Link to='/'>About</Link>
-        <Link to='/login'>
-          <button type='button' className='py-2 px-4 rounded-3xl bg-blue-800'>
-            Login 👋
-          </button>
-        </Link>
+        <SignedOut>
+          <Link to='/login'>
+            <button type='button' className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>
+              Login 👋
+            </button>
+          </Link>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
