ab83fb25180249b3d1a965b414afc6df763805f9
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-24 17:02:24 +0100

    fix: refactor

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import PostList from './routes/PostList';
@@ -9,32 +9,39 @@ import Write from './routes/Write';
 import SinglePost from './routes/SinglePost';
 import Home from './routes/Home.jsx';
 
+import MainLayout from './layouts/MainLayout.jsx';
+
 import './index.css';
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: 'posts',
-    element: <PostList />,
-  },
-  {
-    path: ':slug',
-    element: <SinglePost />,
-  },
-  {
-    path: 'write',
-    element: <Write />,
-  },
-  {
-    path: 'login',
-    element: <Login />,
-  },
-  {
-    path: 'register',
-    element: <Register />,
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: 'posts',
+        element: <PostList />,
+      },
+      {
+        path: ':slug',
+        element: <SinglePost />,
+      },
+      {
+        path: 'write',
+        element: <Write />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+    ],
   },
 ]);
 
