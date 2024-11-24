52071b00a2113a139a5817790c8751581291a53a
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-24 02:57:05 +0100

    feat: create Image component

---
+++ src/components/Image.jsx
@@ -0,0 +1,20 @@
+import { IKImage } from 'imagekitio-react';
+
+const Image = ({ src, className, width, height, alt }) => {
+  return (
+    <IKImage
+      urlEndpoint={import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT}
+      path={src}
+      // path='/logo.png'
+      // src='https://ik.imagekit.io/devayo/logo.png'
+      className={className}
+      loading='lazy'
+      lqip={{ active: true, quality: 20 }}
+      alt={alt}
+      width={width}
+      height={height}
+    />
+  );
+};
+
+export default Image;
