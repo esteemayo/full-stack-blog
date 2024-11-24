422dc7e94ae9bbd0c74d2b862a6b261334b39a16
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-24 01:52:13 +0100

    feat: add html markup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,14 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='w-full h-16 md:h-20 flex items-center justify-between'>
+      <div className='flex items-center gap-4 text-2xl font-bold'>
+        <img src='/logo.png' alt='logo' className='w-8 h-8' />
+        <span>blog</span>
+      </div>
+      <div className='md:hidden'>M</div>
+      <div className='hidden md:flex'>D</div>
+    </nav>
+  );
 };
 
 export default Navbar;
