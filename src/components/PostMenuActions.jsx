11e7d1a65b910471c682247792242b802cd9804d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-24 20:06:49 +0100

    feat: add html markup

--- src/components/PostMenuActions.jsx
+++ src/components/PostMenuActions.jsx
@@ -1,5 +1,25 @@
 const PostMenuActions = () => {
-  return <div>PostMenuActions</div>;
+  return (
+    <div className=''>
+      <h1 className='capitalize'>Actions</h1>
+      <div className='flex items-center gap-2 py-2 text-sm cursor-pointer'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 48 48'
+          width='20px'
+          height='20px'
+        >
+          <path
+            d='M12 4C10.3 4 9 5.3 9 7v34l15-9 15 9V7c0-1.7-1.3-3-3-3H12z'
+            stroke='black'
+            strokeWidth='2'
+          />
+        </svg>
+        <span>Save this Post</span>
+        <span className='text-xs'>(in progress)</span>
+      </div>
+    </div>
+  );
 };
 
 export default PostMenuActions;
