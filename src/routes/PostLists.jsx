d90b8825bafb54e5d2f296a1507c8d5d62bcc4b4
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-25 01:23:09 +0100

    feat: add handleToggle function

--- src/routes/PostLists.jsx
+++ src/routes/PostLists.jsx
@@ -1,15 +1,36 @@
+import { useMemo, useState } from 'react';
+
 import PostList from '../components/PostList';
 import SideMenu from '../components/SideMenu';
 
 const PostLists = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen((value) => {
+      return !value;
+    });
+  };
+
+  const btnLabel = useMemo(() => {
+    return isOpen ? 'Close' : 'Filter or Search';
+  }, [isOpen]);
+
   return (
     <div className=''>
       <h1 className='mb-8 text-2xl capitalize'>Development blogs</h1>
-      <div className='flex gap-8'>
+      <button
+        type='button'
+        onClick={handleToggle}
+        className='bg-blue-800 text-sm text-white px-4 py-2 rounded-xl outline-0 mb-4 md:hidden'
+      >
+        {btnLabel}
+      </button>
+      <div className='flex flex-col-reverse gap-8 md:flex-row'>
         <div className=''>
           <PostList />
         </div>
-        <div className=''>
+        <div className={`${isOpen ? 'block' : 'hidden'} md:block`}>
           <SideMenu />
         </div>
       </div>
