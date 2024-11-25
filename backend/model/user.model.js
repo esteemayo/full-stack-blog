e8600618be70fe23ba5e860698b645bb7d74a1d4
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-25 21:33:51 +0100

    feat: create a user model schema

---
+++ backend/model/user.model.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, 'Please provide your username'],
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide your email address'],
+      unique: true,
+      trim: true,
+      lowerCase: true,
+      validate: {
+        validator: function (val) {
+          const emailRegex =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return emailRegex.test(val);
+        },
+        message: 'Please enter a valid email address',
+      },
+    },
+    img: {
+      type: String,
+    },
+    savedPosts: {
+      type: [String],
+      default: [],
+    },
+    role: {
+      type: String,
+      enum: {
+        values: ['admin', 'user'],
+        message: 'Role is either: admin or user',
+      },
+      default: 'user',
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
