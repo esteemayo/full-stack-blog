7d30ad492ca85558119d8216a61a0e32171a34ac
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-25 02:13:17 +0100

    feat: setup post and comment route s

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 import express from 'express';
 
 import userRoute from './routes/user.route.js';
+import postRoute from './routes/post.route.js';
+import commentRoute from './routes/comment.route.js';
 
 const app = express();
 
 app.use('/api/v1/users', userRoute);
+app.use('/api/v1/posts', postRoute);
+app.use('/api/v1/comments', commentRoute);
 
 export default app;
