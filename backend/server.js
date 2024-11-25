87659be99b7ddce81a04b9ced1d04f647a7b15ba
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-25 20:54:57 +0100

    feat: set up database connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import app from './app.js';
+import { connectDB } from './libs/connectDB.js';
 
 app.set('port', process.env.PORT || 3000);
 
 const server = app.listen(app.get('port'), async () => {
+  await connectDB();
   console.log(`Server is running on port ${server.address().port}!`);
 });
