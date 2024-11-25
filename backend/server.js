59b60abe65186b9c3b566ea6827341a8e28bd40b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-25 20:33:23 +0100

    fix: modify

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,5 @@ import app from './app.js';
 app.set('port', process.env.PORT || 3000);
 
 const server = app.listen(app.get('port'), async () => {
-  console.log(`Server is running on port ${server.address().port}`);
+  console.log(`Server is running on port ${server.address().port}!`);
 });
