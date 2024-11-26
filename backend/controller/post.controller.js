a4efd71a4e2e9c4a4b87dae002126020003d5996
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-26 01:01:48 +0100

    feat: add a route endpoint to create a new post

---
+++ backend/controller/post.controller.js
@@ -0,0 +1,4 @@
+import { StatusCodes } from 'http-status-codes';
+import asyncHandler from 'express-async-handler';
+
+export const createPost = asyncHandler(async (req, res, next) => {});
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,5 +1,9 @@
 import express from 'express';
 
+import * as postController from '../controller/post.controller.js';
+
 const router = express.Router();
+
+router.route('/').post(postController.createPost);
 
 export default router;
