21f62e93049910c8366996d5ae0a43d8c677e6cc
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-26 01:16:22 +0100

    feat: handle fetch posts logic

--- backend/controller/post.controller.js
+++ backend/controller/post.controller.js
@@ -1,6 +1,12 @@
 import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 
-export const getPosts = asyncHandler(async (req, res, next) => {});
+import Post from '../model/post.model.js';
+
+export const getPosts = asyncHandler(async (req, res, next) => {
+  const posts = await Post.find();
+
+  return res.status(StatusCodes.OK).json(posts);
+});
 
 export const createPost = asyncHandler(async (req, res, next) => {});
