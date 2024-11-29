8e6e3256f84bc88f6dddc0557b98788f260fe709
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-29 16:07:51 +0100

    feat: add ToasterProvider

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 
 import Navbar from '../components/Navbar';
+import ToasterProvider from '../providers/ToasterProvider';
 
 const MainLayout = () => {
   return (
     <div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-56'>
       <Navbar />
+      <ToasterProvider />
       <Outlet />
     </div>
   );
