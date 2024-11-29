8081f40cbfd374467f2439725559017b5a05016f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-29 16:27:42 +0100

    feat: add api endpoint to create a new post

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -3,3 +3,5 @@ import http from './httpService';
 const apiEndpoint = '/posts';
 
 export const getPosts = () => http.get(apiEndpoint);
+
+export const createPost = (post) => http.post(apiEndpoint, post);
