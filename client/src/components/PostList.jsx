8bb026e3c708fdde70ed8d7c89eb67dc8b6a37b9
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-29 16:26:00 +0100

    feat: fetch posts from the server

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,6 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+
 import PostListItem from './PostListItem';
+import { getPosts } from '../services/postService';
+
+const fetchPosts = async () => {
+  const { data } = await getPosts();
+  return data;
+};
 
 const PostList = () => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ['posts'],
+    queryFn: () => fetchPosts(),
+  });
+
+  if (isPending) return 'Loading...';
+
+  if (error) return `An error has occurred ${error.message}`;
+
   return (
     <>
       <PostListItem />
