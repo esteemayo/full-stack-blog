c838e8a88a454cc4704b404af585279029c4243b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-11-29 17:29:23 +0100

    feat: handle create new post functionality

--- client/src/routes/Write.jsx
+++ client/src/routes/Write.jsx
@@ -1,12 +1,57 @@
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useAuth, useUser } from '@clerk/clerk-react';
 import ReactQuill from 'react-quill-new';
-import { useUser } from '@clerk/clerk-react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import Upload from '../components/Upload';
 
 import 'react-quill-new/dist/quill.snow.css';
+import http from '../services/httpService';
+import { toast } from 'react-toastify';
 
 const Write = () => {
+  const queryClient = useQueryClient();
+
+  const { getToken } = useAuth();
   const { isLoaded, isSignedIn } = useUser();
+
+  const navigate = useNavigate();
+
+  const { isError, isPending, error, mutate } = useMutation({
+    mutationFn: async (post) => {
+      const token = await getToken();
+      const { data } = await http.post('/posts', post, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      return data;
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ['posts'] });
+      toast.success('Post has been created');
+      navigate(`/${data.slug}`);
+    },
+  });
+
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const data = {
+      title: formData.get('title'),
+      desc: formData.get('desc'),
+      category: formData.get('category'),
+      content: formData.get('content'),
+    };
+
+    mutate(data);
+  };
 
   if (!isLoaded) {
     return <div className=''>Loading...</div>;
@@ -19,7 +64,7 @@ const Write = () => {
   return (
     <div className='h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] flex flex-col gap-6'>
       <h1 className='text-cl font-light'>Create a New Post</h1>
-      <form className='flex flex-col gap-6 flex-1 mb-6'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-6 flex-1 mb-6'>
         <Upload>
           <button
             type='button'
@@ -63,15 +108,19 @@ const Write = () => {
           </div>
           <ReactQuill
             theme='snow'
+            value={value}
+            onChange={setValue}
             className='flex-1 rounded-xl bg-white shadow-md'
           />
         </div>
         <button
           type='button'
+          disabled={!!isPending}
           className='bg-blue-800 text-white font-medium rounded-xl mt-4 p-2 w-36 disabled:bg-blue-400 disabled:cursor-not-allowed'
         >
-          Send
+          {isPending ? 'Loading...' : 'Send'}
         </button>
+        {isError && <span>{error.message}</span>}
       </form>
     </div>
   );
