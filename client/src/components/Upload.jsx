c4f6057a8f91a92beb34891041a96d3259401b77
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-01 01:45:10 +0100

    fix: refactor

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,5 +1,69 @@
-const Upload = ({ children }) => {
-  return <div>{children}</div>;
+import { toast } from 'react-toastify';
+import { useRef } from 'react';
+import { IKContext, IKUpload } from 'imagekitio-react';
+
+const publicKey = import.meta.env.VITE_IMAGEKIT_PUBLIC_KEY;
+const urlEndpoint = import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT;
+
+const authenticator = async () => {
+  try {
+    const response = await fetch(
+      'http://localhost:3030/api/v1/posts/upload-auth'
+    );
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`
+      );
+    }
+
+    const data = await response.json();
+    const { signature, expire, token } = data;
+    return { signature, expire, token };
+  } catch (error) {
+    throw new Error(`Authentication request failed: ${error.message}`);
+  }
+};
+
+const Upload = ({ children, type, setData, setProgress }) => {
+  const ref = useRef(null);
+
+  const onError = (err) => {
+    console.log(err);
+    toast.error('Image upload failed!');
+  };
+
+  const onSuccess = (res) => {
+    console.log(res);
+    setData(res);
+  };
+
+  const onUploadProgress = (progress) => {
+    console.log(progress);
+    setProgress(Math.round(progress.loaded / progress.total) * 100);
+  };
+
+  return (
+    <IKContext
+      publicKey={publicKey}
+      urlEndpoint={urlEndpoint}
+      authenticator={authenticator}
+    >
+      <IKUpload
+        useUniqueFileName
+        onError={onError}
+        onSuccess={onSuccess}
+        onUploadProgress={onUploadProgress}
+        className='hidden'
+        ref={ref}
+        accept={`${type}/*`}
+      />
+      <div className='cursor-pointer' onClick={() => ref.current.click}>
+        {children}
+      </div>
+    </IKContext>
+  );
 };
 
 export default Upload;
