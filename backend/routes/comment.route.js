afa6e18f701316caa752fd68d0d240dff9c7f0e1
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-02 17:39:35 +0100

    feat: add endpoint to fetch all post comments

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -1,5 +1,9 @@
 import express from 'express';
 
+import * as commentController from '../controllers/comment.controller.js';
+
 const router = express.Router();
+
+router.get('/:postId', commentController.getPostComments);
 
 export default router;
