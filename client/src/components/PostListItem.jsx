bbcbd8364402c2dc587926c65f9e1235775b663b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-02 12:34:19 +0100

    feat: render post item dynamically

--- client/src/components/PostListItem.jsx
+++ client/src/components/PostListItem.jsx
@@ -1,38 +1,35 @@
+import { format } from 'timeago.js'
 import { Link } from 'react-router-dom';
 
 import Image from './Image';
 
-const PostListItem = () => {
+const PostListItem = ({ title, desc, img, slug, user, category, createdAt }) => {
   return (
     <article className='flex flex-col xl:flex-row gap-8 mb-12'>
       <div className='md:hidden xl:block xl:w-1/3'>
-        <Image
-          src='postImg.jpeg'
+        {img && <Image
+          src={img}
           className='rounded-2xl object-cover'
           width='735'
-        />
+        />}
       </div>
       <div className='flex flex-col gap-4 xl:w-2/3'>
-        <Link to='/test' className='text-4xl font-semibold'>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam modi
-          eum aut.
+        <Link to={`/${slug}`} className='text-4xl font-semibold'>
+          {title}
         </Link>
         <div className='flex items-center gap-2 text-gray-400 text-sm'>
           <span>Written by</span>
           <Link className='text-blue-800 capitalize' to=''>
-            John doe
+            {user.username}
           </Link>
           <span>on</span>
-          <Link className='text-blue-800 capitalize'>Web design</Link>
-          <span></span>
+          <Link className='text-blue-800 capitalize'>{category}</Link>
+          <span>{format(createdAt)}</span>
         </div>
         <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam vel
-          quia perspiciatis corporis nostrum. Dolorem a quibusdam porro?
-          Suscipit amet nisi officiis dicta, id ab aliquid nobis? Assumenda,
-          rerum ab.
+          {desc}
         </p>
-        <Link to='/test' className='underline text-blue-800 text-sm capitalize'>
+        <Link to={`/${slug}`} className='underline text-blue-800 text-sm capitalize'>
           Read more
         </Link>
       </div>
