54181730e6e32c3d58ecb6a6bd8fe42849996348
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-02 22:51:10 +0100

    fix: modify

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -2,26 +2,28 @@ import { format } from 'timeago.js';
 
 import Image from './Image';
 
-const Comment = ({ desc, user, createdAt }) => {
+const Comment = ({ comment }) => {
   return (
     <div className='p-4 bg-slate-50 rounded-xl mb-8'>
       <div className='flex items-center gap-4'>
-        {user.img && (
+        {comment.user.img && (
           <Image
-            src={user.img}
+            src={comment.user.img}
             className='w-10 h-10 rounded-full object-cover'
             width='40'
           />
         )}
-        <span className='font-medium capitalize'>{user.username}</span>
-        <span className='text-sm text-gray-500'>{format(createdAt)}</span>
+        <span className='font-medium capitalize'>{comment.user.username}</span>
+        <span className='text-sm text-gray-500'>
+          {format(comment.createdAt)}
+        </span>
         <span className='text-xs text-red-300 hover:text-red-500 cursor-pointer'>
           delete
           <span>(in progress)</span>
         </span>
       </div>
       <div className='mt-4'>
-        <p>{desc}</p>
+        <p>{comment.desc}</p>
       </div>
     </div>
   );
