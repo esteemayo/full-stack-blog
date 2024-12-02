c9794d0ac9e7b783e095fe32548a6185103b9dbd
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-02 17:54:45 +0100

    feat: handle create new comment logic

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -1,6 +1,9 @@
 import { StatusCodes } from 'http-status-codes';
 
+import User from '../model/user.model.js';
 import Comment from '../model/comment.model.js';
+
+import { UnauthenticatedError } from '../errors/unauthenticated.error.js';
 
 export const getPostComments = async (req, res) => {
   const { postId } = req.params;
@@ -10,4 +13,22 @@ export const getPostComments = async (req, res) => {
     .sort('-createdAt');
 
   return res.status(StatusCodes.OK).json(comments);
+};
+
+export const addComment = async (req, res, next) => {
+  const { postId } = req.params;
+  const clerkUserId = req.auth.userId;
+
+  if (!clerkUserId) {
+    return next(new UnauthenticatedError('Not authenticated'));
+  }
+
+  const user = await User.findOne({ clerkUserId });
+
+  if (!req.body.user) req.body.user = user._id;
+  if (!req.body.post) req.body.post = postId;
+
+  const comment = await Comment.create({ ...req.body });
+
+  return res.status(StatusCodes.CREATED).json(comment);
 };
