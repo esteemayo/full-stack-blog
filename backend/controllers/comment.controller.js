64be60e8bc431668b5f94ff775c84fd2c96c04a2
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-02 17:47:48 +0100

    fix: modify

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -5,7 +5,9 @@ import Comment from '../model/comment.model.js';
 export const getPostComments = async (req, res) => {
   const { postId } = req.params;
 
-  const comments = await Comment.find({ post: postId });
+  const comments = await Comment.find({ post: postId })
+    .populate('user', 'username img')
+    .sort('-createdAt');
 
   return res.status(StatusCodes.OK).json(comments);
 };
