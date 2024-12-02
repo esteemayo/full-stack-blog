a57bfbcc4ce2a4d08041267483222a4010cb02b1
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-02 22:43:37 +0100

    feat: render comment component dynamically

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,28 +1,27 @@
+import { format } from 'timeago.js';
+
 import Image from './Image';
 
-const Comment = () => {
+const Comment = ({ desc, user, createdAt }) => {
   return (
     <div className='p-4 bg-slate-50 rounded-xl mb-8'>
       <div className='flex items-center gap-4'>
-        <Image
-          src='userImg.jpeg'
-          className='w-10 h-10 rounded-full object-cover'
-          width='40'
-        />
-        <span className='font-medium capitalize'>John doe</span>
-        <span className='text-sm text-gray-500'>2 days ago</span>
+        {user.img && (
+          <Image
+            src={user.img}
+            className='w-10 h-10 rounded-full object-cover'
+            width='40'
+          />
+        )}
+        <span className='font-medium capitalize'>{user.username}</span>
+        <span className='text-sm text-gray-500'>{format(createdAt)}</span>
         <span className='text-xs text-red-300 hover:text-red-500 cursor-pointer'>
           delete
           <span>(in progress)</span>
         </span>
       </div>
       <div className='mt-4'>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum optio
-          debitis laboriosam perferendis tenetur temporibus iure, sed quas,
-          ratione deleniti totam unde vitae, recusandae eius nulla?
-          Necessitatibus eveniet eius minima.
-        </p>
+        <p>{desc}</p>
       </div>
     </div>
   );
