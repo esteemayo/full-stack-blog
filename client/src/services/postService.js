06a2beb0864b233405fd399a73b3a416cd94e173
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-02 12:38:27 +0100

    feat: add api endpoint to fetch a single post

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -7,4 +7,6 @@ export const getPosts = (pageParam) =>
     params: { page: pageParam, limit: 2 },
   });
 
+export const getPost = (slug) => http.get(`${apiEndpoint}/${slug}`);
+
 export const createPost = (post) => http.post(apiEndpoint, post);
