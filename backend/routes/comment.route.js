088edf2a5151895ea38da2e271bd90dea2add05d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-02 17:43:06 +0100

    feat: add endpoint to create a comment on a post

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -4,6 +4,9 @@ import * as commentController from '../controllers/comment.controller.js';
 
 const router = express.Router();
 
-router.get('/:postId', commentController.getPostComments);
+router
+  .route('/:postId')
+  .get(commentController.getPostComments)
+  .post(commentController.addPost);
 
 export default router;
