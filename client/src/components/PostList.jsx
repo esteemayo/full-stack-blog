c0b78d09fee6e3eb10a642be18b179d8c26da39b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-02 12:23:05 +0100

    feat: implement infinite scroll

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,37 +1,47 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import React from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 import PostListItem from './PostListItem';
 import { getPosts } from '../services/postService';
 
-const fetchPosts = async () => {
-  const { data } = await getPosts();
+const fetchPosts = async (pageParam) => {
+  const { data } = await getPosts(pageParam);
   return data;
 };
 
 const PostList = () => {
-  const { isPending, error, data } = useQuery({
-    queryKey: ['posts'],
-    queryFn: () => fetchPosts(),
-  });
+  const { data, error, fetchNextPage, hasNextPage, isFetching } =
+    useInfiniteQuery({
+      queryKey: ['posts'],
+      queryFn: ({ pageParam = 1 }) => fetchPosts(pageParam),
+      initialPageParam: 1,
+      getNextPageParam: (lastPage, pages) =>
+        lastPage.hasMore ? pages.length + 1 : undefined,
+    });
 
-  if (isPending) return 'Loading...';
+  if (isFetching) return 'Loading...';
 
   if (error) return `An error has occurred ${error.message}`;
 
+  const allPosts = data?.pages.flatMap((page) => page.posts) || [];
+
   return (
-    <>
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-    </>
+    <InfiniteScroll
+      dataLength={allPosts.length}
+      next={fetchNextPage}
+      hasMore={!!hasNextPage}
+      loader={<h4>Loading more posts...</h4>}
+      endMessage={
+        <p>
+          <b>All posts loaded!</b>
+        </p>
+      }
+    >
+      {allPosts.map((post) => {
+        return <PostListItem key={post._id} {...post} />;
+      })}
+    </InfiniteScroll>
   );
 };
 
