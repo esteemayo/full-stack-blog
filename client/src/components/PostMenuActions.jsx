8cac562afbc45fea4919b533b5d5e34fa4ebfb98
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-03 20:46:20 +0100

    feat: handle save post functionality

--- client/src/components/PostMenuActions.jsx
+++ client/src/components/PostMenuActions.jsx
@@ -1,28 +1,57 @@
+import { useQuery } from '@tanstack/react-query';
 import { useAuth, useUser } from '@clerk/clerk-react';
+
+import { getSavedPosts } from '../services/userService';
+
+const fetchSavedPosts = async (token) => {
+  const { data } = await getSavedPosts(token);
+  return data;
+};
 
 const PostMenuActions = ({ post }) => {
   const { user } = useUser();
   const { getToken } = useAuth();
 
+  const {
+    isPending,
+    error,
+    data: savedPosts,
+  } = useQuery({
+    queryKey: ['savedPosts'],
+    queryFn: async () => {
+      const token = getToken();
+      fetchSavedPosts(token);
+    },
+  });
+
+  const isSaved = savedPosts?.some((p) => p === post._id) || false;
+
   return (
     <div className=''>
       <h1 className='capitalize'>Actions</h1>
-      <div className='flex items-center gap-2 py-2 text-sm cursor-pointer'>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 48 48'
-          width='20px'
-          height='20px'
-        >
-          <path
-            d='M12 4C10.3 4 9 5.3 9 7v34l15-9 15 9V7c0-1.7-1.3-3-3-3H12z'
-            stroke='black'
-            strokeWidth='2'
-          />
-        </svg>
-        <span>Save this Post</span>
-        <span className='text-xs'>(in progress)</span>
-      </div>
+      {isPending ? (
+        'Loading...'
+      ) : error ? (
+        'Saved post fetching failed'
+      ) : (
+        <div className='flex items-center gap-2 py-2 text-sm cursor-pointer'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 48 48'
+            width='20px'
+            height='20px'
+          >
+            <path
+              d='M12 4C10.3 4 9 5.3 9 7v34l15-9 15 9V7c0-1.7-1.3-3-3-3H12z'
+              stroke='black'
+              strokeWidth='2'
+              fill={isSaved ? 'black' : 'none'}
+            />
+          </svg>
+          <span>Save this Post</span>
+          <span className='text-xs'>(in progress)</span>
+        </div>
+      )}
       <div className='flex items-center gap-2 py-2 text-sm cursor-pointer'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
