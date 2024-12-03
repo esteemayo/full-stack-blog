a4f09f6fe949c264ab0f6c954af451ab39d346d3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-03 20:19:22 +0100

    feat: handle the logic to fetch authenticated user' saved posts

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,2 +1,17 @@
 import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
+
+import User from '../model/user.model.js';
+import { UnauthenticatedError } from '../errors/unauthenticated.error.js';
+
+export const getUserSavedPosts = async (req, res, next) => {
+  const clerkUserId = req.auth.userId;
+
+  if (!clerkUserId) {
+    return next(new UnauthenticatedError('Not authenticated!'));
+  }
+
+  const user = await User.findOne({ clerkUserId });
+
+  return res.status(StatusCodes.OK).json(user.savedPosts);
+};
