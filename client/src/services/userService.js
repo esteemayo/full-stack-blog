70827ed325688d9f098b7822041d81b61d920d04
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-03 20:37:08 +0100

    feat: add endpoint to fetch authenticated user saved post

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -2,6 +2,13 @@ import http from './httpService';
 
 const apiEndpoint = '/users';
 
+export const getSavedPosts = (token) =>
+  http.get(`${apiEndpoint}/saved`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
 export const savePost = (postId, token) =>
   http.patch(`${apiEndpoint}/save`, postId, {
     headers: {
