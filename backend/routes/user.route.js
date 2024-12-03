a386455a2bad4461f7b9526097b40fd9712f686a
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-03 20:13:38 +0100

    feat: create api route endpoint to fetch and save posts for users

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,11 @@
 import express from 'express';
 
+import * as userController from '../controllers/user.controller.js';
+
 const router = express.Router();
+
+router.get('/saved', userController.getUserSavedPosts);
+
+router.patch('/save', userController.SavePost);
 
 export default router;
