c7a3d5fe14e0f41ada36578d218dfe20625b754a
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-05 20:53:35 +0100

    fix: modify

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -2,10 +2,14 @@ import http from './httpService';
 
 const apiEndpoint = '/posts';
 
-export const getPosts = (pageParam) =>
+export const getPosts = (pageParam, searchParams) => {
+  const searchParamsObj = Object.fromEntries([...searchParams]);
+  // console.log(searchParamsObj);
+
   http.get(apiEndpoint, {
-    params: { page: pageParam, limit: 2 },
+    params: { page: pageParam, limit: 2, searchParamsObj },
   });
+};
 
 export const getPost = (slug) => http.get(`${apiEndpoint}/${slug}`);
 
